bf52b96c4882481e74c1228cedc9fdd1706f354f
65160242 <alisawiwalisawiw4849@gmail.com>
2024-01-09 14:10:23 +0700

    ตามอาจารย์

---
+++ math.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <meta charset="uft-8">
+</head>
+
+<body>
+<?php $multi_x = 4; ?>
+<h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>    
+
+<table>
+    <tbody>
+        <tr>
+            <td></td>
+            <td>x</td>
+            <td></td>
+            <td>=</td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+<style>
+    table{
+        width: 150px;
+        height: 265px;
+    }
+</style>
+</body>
+</html>
